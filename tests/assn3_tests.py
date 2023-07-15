68d7d11d702a2f5c070d7290af0d74d4ab580190
Spencer Miller <spencer.miller@me.com>
2023-07-15 16:49:59 +0300

    bug fix

--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -5,4 +5,4 @@ MEALS_SERVICE_BASE_URL = 'http://127.0.0.1:8000'
 def test_sanity_check():
     res = requests.get(MEALS_SERVICE_BASE_URL)
 
-    assert res.json() == 'OK'
+    assert res.text == 'OK'
