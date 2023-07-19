be21e4855ca815a80e7f8c07e13cde48e6e5fc12
Spencer Miller <spencer.miller@me.com>
2023-07-19 18:46:22 +0300

    bug fix

--- query.sh
+++ query.sh
@@ -1,7 +1,6 @@
-# while IFS= read -r line
-# do
-#     echo "http://127.0.0.1:8000/dishes/${line::-3}"
-#     curl -X GET -v --location --request --ipv4 "http://127.0.0.1:8000/dishes/${line::-3}"
-# done < query.txt
-
-echo $(curl -v --request --ipv4 http://0.0.0.0:8000)
+while IFS= read -r line
+do
+    echo http://127.0.0.1:8000/dishes/${line::-3}
+    echo {"name": ${line::-3}}
+    # curl -X GET -v --location --request --ipv4 "http://127.0.0.1:8000/dishes/${line::-3}"
+done < query.txt
