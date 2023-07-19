a29e6e3f898e289a0298959e4d2505ff29a302f6
Spencer Miller <spencer.miller@me.com>
2023-07-19 08:20:46 +0300

    added post request to query job

--- query.sh
+++ query.sh
@@ -1,5 +1,7 @@
-while IFS= read -r line
-do
-    echo "http://127.0.0.1:8000/dishes/${line::-3}"
-    curl -X GET -v --location --request --ipv4 "http://127.0.0.1:8000/dishes/${line::-3}"
-done < query.txt
+# while IFS= read -r line
+# do
+#     echo "http://127.0.0.1:8000/dishes/${line::-3}"
+#     curl -X GET -v --location --request --ipv4 "http://127.0.0.1:8000/dishes/${line::-3}"
+# done < query.txt
+
+echo $(curl -v --request --ipv4 http://0.0.0.0:8000)
