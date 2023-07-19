77cd0e9ae943cb3c1f701aa8d3dbfa140e5fa374
Spencer Miller <spencer.miller@me.com>
2023-07-19 07:59:50 +0300

    bug fix

---
+++ query.sh
@@ -0,0 +1,5 @@
+while IFS= read -r line
+do
+    echo "http://127.0.0.1:8000/dishes/${line::-3}"
+    curl -X GET -v --location --request --ipv4 "http://127.0.0.1:8000/dishes/${line::-3}"
+done < query.txt
